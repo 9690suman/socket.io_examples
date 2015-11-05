a40de0b08fd7efa9c30bebe97194171483d78a73
9690suman <sarnendu.mukherjee@gmail.com>
2015-11-05 22:40:51 +0530

    1. Broadcasted a message to connected users when someone connects or disconnects. 2. Added support for nicknames. 4. Added “{user} is typing” functionality.

--- CHAT/index.js
+++ CHAT/index.js
@@ -1,12 +1,31 @@
 var express = require('express');
-
-
 var app = express();
+var http = require('http').Server(app)
+
+var io = require('socket.io')(http)
 
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(3000,function(){
+var onlineUsers = [];
+io.on('connection', function(socket){
+	//io.emit('chat message', 'Someone Connected!');
+	socket.on('chat message', function(msg){
+		io.emit('chat message', msg);
+	}).on('chat message joined', function(nickname){
+		io.emit('chat message', nickname+' joined!');
+	}).on('chat message left', function(nickname){
+		io.emit('chat message', nickname+' left!');
+	}).on('chat message typing', function(nickname){
+		io.emit('chat message typing', nickname+' typing...!');
+	});
+	
+	/*socket.on('disconnect', function(){
+		io.emit('chat message', 'Someone Disconnected!');
+	});*/
+});
+
+http.listen(3000,function(){
 	console.log('listning on 3000');
 });
